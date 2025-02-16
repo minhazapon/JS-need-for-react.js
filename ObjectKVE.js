3b3cef027eedaf2e5180336b1d4484c3a8f57f67
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-15 20:26:37 -0800

    added file

---
+++ ObjectKVE.js
@@ -0,0 +1,18 @@
+
+
+
+//object keys
+
+const info1 = {
+
+    name: 'minhazul abedin apon',
+    age: 23,
+    profession: "software developer",
+    address: 'jhenaidah',
+    position: 'react.js engineer',
+    gender: 'male',
+    school: 'jhenaidah govt. high school'
+
+}
+
+console.log(Object.keys(info1))
