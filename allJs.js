8e1ac17b0c45d37567e48687221d784e3d53c9fc
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-23 01:50:11 -0800

    added file

---
+++ allJs.js
@@ -0,0 +1,13 @@
+
+
+//let , const , var
+
+let name1 = "minhazul abedin apon"
+console.log(name1);
+
+const name2 = "apon the javaScript developer"
+console.log(name2)
+
+var skill = "react.js"
+console.log(skill)
+
